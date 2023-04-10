8e9c3765300feafcb8cc5025fe4bbc9e0ec98e57
Newtayo <akandeabdul@gmail.com>
2023-04-10 12:13:21 +0100

    Add Navlink with style for active link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import planet from '../images/planet.png';
+import '../styles/Nav.css';
 
 export default function Nav() {
   return (
-    <div className="nav-bar">
+    <nav className="nav-bar">
       <img src={planet} alt="Logo" />
       <h1>Space travelers Hub</h1>
-    </div>
+      <NavLink className="nav-linkfirst" to="/">Rockets</NavLink>
+      <NavLink className="nav-linksecond" to="/mission">Missions</NavLink>
+      <NavLink className="nav-linkthird" to="/profile">My Profile</NavLink>
+    </nav>
   );
 }
