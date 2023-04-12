308350c8cbadb499ed6e79cc5c2c7f67c5056e42
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-04-12 09:16:57 +0300

    Add cancelation reducer

--- src/redux/rocketsSlice.js
+++ src/redux/rocketsSlice.js
@@ -3,29 +3,38 @@ import axios from 'axios';
 
 const FETCH_ROCKETS = 'FETCH_ROCKETS_STATUS';
 const RESERVE_ROCKETS = 'RESERVE_ROCKETS';
+const CANCEL_RESERVATION = 'CANCEL_RESERVATION';
 
 const fetchRockets = async () => {
   const response = await axios.get('https://api.spacexdata.com/v3/rockets');
   return response.data;
 };
 
-export const retrieveRockets = createAsyncThunk(FETCH_ROCKETS, async () => {
-  const response = await fetchRockets();
-  const rocketsData = [];
-  response.forEach((rocket) => {
-    const singleRocket = {
-      id: rocket.id,
-      name: rocket.rocket_name,
-      description: rocket.description,
-      image: rocket.flickr_images[0],
-      reserved: false,
-    };
-    rocketsData.push(singleRocket);
-  });
-  return rocketsData;
+export const retrieveRockets = createAsyncThunk(FETCH_ROCKETS, async (obj, thunkAPI) => {
+  const currentState = thunkAPI.getState();
+  if (currentState.rocketsReducer.length === 0) {
+    const response = await fetchRockets();
+    const rocketsData = [];
+    response.forEach((rocket) => {
+      const singleRocket = {
+        id: rocket.id,
+        name: rocket.rocket_name,
+        description: rocket.description,
+        image: rocket.flickr_images[0],
+        reserved: false,
+      };
+      rocketsData.push(singleRocket);
+    });
+    return rocketsData;
+  }
+  return currentState.rocketsReducer;
 });
 
 export const reserveRocket = createAction(RESERVE_ROCKETS, (id) => ({
+  payload: id,
+}));
+
+export const cancelReservation = createAction(CANCEL_RESERVATION, (id) => ({
   payload: id,
 }));
 
@@ -36,6 +45,9 @@ const rockets = createReducer(INITIAL_STATE, ((builder) => {
     .addCase(retrieveRockets.fulfilled, ((state, action) => action.payload))
     .addCase(reserveRocket, ((state, action) => state.map((rocket) => (
       rocket.id === action.payload ? { ...rocket, reserved: true } : rocket
+    ))))
+    .addCase(cancelReservation, ((state, action) => state.map((rocket) => (
+      rocket.id === action.payload ? { ...rocket, reserved: false } : rocket
     ))))
     .addDefaultCase((state) => [...state]);
 }));
