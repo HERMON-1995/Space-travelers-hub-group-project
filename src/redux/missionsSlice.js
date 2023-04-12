b5c20d58a3ab422bae410a6dd7bbbf4003b30500
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-04-12 20:23:14 +0300

    Update missionsSlice.js

--- src/redux/missionsSlice.js
+++ src/redux/missionsSlice.js
@@ -1,5 +1,4 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 
 const url = 'https://api.spacexdata.com/v3/missions';
 
@@ -14,8 +13,12 @@ const initialState = {
 
 export const getMission = createAsyncThunk('missionList/getMission', async () => {
   try {
-    const response = await axios.get(url);
-    return response.data;
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
   } catch (error) {
     return error;
   }
