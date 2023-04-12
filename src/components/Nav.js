917e9423b2dc820edf181d137500372071eeb106
Newtayo <akandeabdul@gmail.com>
2023-04-12 16:30:56 +0100

    Display mission of profile page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { getMission } from '../redux/missionsSlice';
 import planet from '../images/planet.png';
 import '../styles/Nav.css';
 
 export default function Nav() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMission());
+  },
+  [dispatch]);
   return (
+
     <nav className="nav-bar">
       <img src={planet} alt="Logo" />
       <h1 className="h4">Space Travelers&apos; Hub</h1>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,13 +3,21 @@ import { useSelector } from 'react-redux';
 
 const Profile = () => {
   const reservedRockts = useSelector((state) => state.rocketsReducer.filter((rck) => rck.reserved));
+  const misprfo = useSelector((state) => state.missionReducer.missionList
+    .filter((mis) => mis.missionValue));
 
   return (
-    <div>
-      <div className="container row col-6">
+    <div className="row">
+      <div className=" col-6">
         <h3 className="ms-5 mt-3">My Rockets</h3>
         <ul className="list-group ms-5">
           {reservedRockts.length ? reservedRockts.map((rocket) => <li key={rocket.id} className="list-group-item">{rocket.name}</li>) : <li className="list-group-item">No Reserved Rockets</li> }
+        </ul>
+      </div>
+      <div className="col-6">
+        <h3 className="ms-5 mt-3">My Missions</h3>
+        <ul className="list-group ms-5">
+          {misprfo.length ? misprfo.map((miss) => <li key={miss.id} className="list-group-item">{miss.missionName}</li>) : <li className="list-group-item">No Mission Reserved</li> }
         </ul>
       </div>
     </div>
